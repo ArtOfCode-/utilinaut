4e9b1932574e2b47149951b2101188100cbd332e
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-03-02 22:46:28 +0000

    add setup and configuration files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'utilinaut',
+    packages = ['utilinaut'],
+    version = '0.0.1',
+    description = 'General utilities for use in Python scripting.',
+    author = 'ArtOfCode',
+    author_email = 'hello@artofcode.co.uk',
+    download_url = 'https://github.com/ArtOfCode-/utilinaut/tarball/0.0.x'
+)
